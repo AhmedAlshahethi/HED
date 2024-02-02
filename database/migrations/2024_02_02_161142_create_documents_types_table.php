05a4f0050ca3552e967de36af0f669d33a7bcfda
Khalid Alshaibani <princofimagination@hotmail.com>
2024-02-02 20:36:15 +0300

    fixed document type table name in migration

--- database/migrations/2024_02_02_161142_create_documents_types_table.php
+++ database/migrations/2024_02_02_161142_create_documents_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('documents_types', function (Blueprint $table) {
+        Schema::create('document_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->softDeletes();
