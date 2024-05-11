379d211fa6c04d918089cf30c36bdd70bde0be2a
Khalid Alshaibani <princofimagination@hotmail.com>
2024-05-11 12:06:20 +0300

    minor changes

--- lang/en/schedules/view_schedule.php
+++ lang/en/schedules/view_schedule.php
@@ -1,13 +1,24 @@
-<?php 
+<?php
 
-return[
+return [
     'New_Schedule' => 'View Schedule Data',
     'Schedule_Name' => 'Schedule Name',
     'Print' => 'Print',
     'Lectures' => 'Lectures',
-    'Lectures_Table' => ['Table_Name' => 'Table_Name','Instructor' => 'Instructor', 'Day' => 'Day', 'Time' => 'Time', 
-    'Subject' => 'Subject', 'Lecture' => 'Lecture',],
-    
-    'Home'=> 'View Schedule',
+    'Lectures_Table' => [
+        'Table_Name' => 'Table_Name', 'Instructor' => 'Instructor', 'Day' => 'Day', 'Time' => 'Time',
+        'Subject' => 'Subject', 'Lecture' => 'Lecture',
+    ],
+    'Days' => [
+        '1' => 'Saturday',
+        '2' => 'Sunday',
+        '3' => 'Monday',
+        '4' => 'Tuesday',
+        '5' => 'Wednesday',
+        '6' => 'Thursday',
+        '7' => 'Friday',
+    ],
+
+    'Home' => 'View Schedule',
     'Screen' => 'Manage Schedules',
 ];
